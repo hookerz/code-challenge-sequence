de56b365135eb3c833caabda478a54ce5c1a25e1
Nicolas Gongora <nicolas@ncodes.dev>
2022-04-13 13:00:28 -0500

    Prepares project for development

---
+++ src/themes/index.ts
@@ -0,0 +1,1 @@
+export { default as MainTheme, MainThemeType } from './mainTheme';
---
+++ src/themes/mainTheme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from 'styled-components';
+
+const defaultTheme: DefaultTheme = {
+  borderRadius: '4px',
+  palette: {
+    common: {
+      black: '#222831',
+      white: '#ffffff',
+    },
+    primary: {
+      main: '#726a95',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#709fb0',
+      contrastText: '#ffffff',
+    },
+  },
+};
+
+export type MainThemeType = typeof defaultTheme;
+
+export default defaultTheme;
